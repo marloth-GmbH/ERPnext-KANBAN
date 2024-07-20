d387588ca8e32b99afd75fe41023e992e873a0ab
Jan Marloth <jm@marloth-tech.com>
2024-07-20 12:25:46 +0200

    changed formats for better readability

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def download_image(image_url):
     except requests.exceptions.RequestException:
         return Image.open("./default.png")  # Return a default image if the image_url is not valid
 
-def draw_text_center(pdf_canvas, text, x, y, width, height):
+def draw_h1_center(pdf_canvas, text, x, y, width, height):
     styles = getSampleStyleSheet()
     style = styles["Normal"]
     style.alignment = TA_CENTER
@@ -96,7 +96,7 @@ def draw_text_center(pdf_canvas, text, x, y, width, height):
     width_needed, height_needed = p.wrap(available_width, available_height)
 
     # Draw the frame with a visible boundary for debugging
-    frame = Frame(x, y, available_width, available_height, showBoundary=0, leftPadding=0, rightPadding=0, topPadding=0, bottomPadding=0)
+    frame = Frame(x, y, available_width, available_height, showBoundary=1, leftPadding=0, rightPadding=0, topPadding=0, bottomPadding=0)
     frame.addFromList([p], pdf_canvas)
 
 def draw_text_left(pdf_canvas, text, x, y, width, height):
@@ -107,19 +107,69 @@ def draw_text_left(pdf_canvas, text, x, y, width, height):
     style.fontSize = 12  # Starting font size
     style.leading = style.fontSize + 2  # Add extra space between lines
 
-    p = Paragraph(text, style)
-    available_width, available_height = width, height
+    # Convert width and height to points (1 mm = 2.83465 points)
+    available_width = width
+    available_height = height
 
+
+    # Create a paragraph and try to fit it in the available space
     while True:
         p = Paragraph(text, style)
         width_needed, height_needed = p.wrap(available_width, available_height)
+    
+        
         if height_needed <= available_height or style.fontSize <= 4:
             break
+        
+        # Reduce font size and try again
         style.fontSize -= 1
+        style.leading = style.fontSize + 2  # Adjust leading accordingly
 
-    frame = Frame(x, y, width, height, showBoundary=1, leftPadding=0, rightPadding=0, topPadding=0, bottomPadding=0)
+    # Check if the paragraph fits in the available space after adjusting font size
+    
+
+    p = Paragraph(text, style)
+    width_needed, height_needed = p.wrap(available_width, available_height)
+
+    # Draw the frame with a visible boundary for debugging
+    frame = Frame(x, y, available_width, available_height, showBoundary=1, leftPadding=0, rightPadding=0, topPadding=0, bottomPadding=0)
     frame.addFromList([p], pdf_canvas)
 
+def draw_text_center(pdf_canvas, text, x, y, width, height):
+    styles = getSampleStyleSheet()
+    style = styles["Normal"]
+    style.alignment = TA_CENTER
+    style.fontName = 'Helvetica'
+    style.fontSize = 20  # Starting font size
+    style.leading = style.fontSize + 2  # Add extra space between lines
+
+    # Convert width and height to points (1 mm = 2.83465 points)
+    available_width = width
+    available_height = height
+
+
+    # Create a paragraph and try to fit it in the available space
+    while True:
+        p = Paragraph(text, style)
+        width_needed, height_needed = p.wrap(available_width, available_height)
+    
+        
+        if height_needed <= available_height or style.fontSize <= 4:
+            break
+        
+        # Reduce font size and try again
+        style.fontSize -= 1
+        style.leading = style.fontSize + 2  # Adjust leading accordingly
+
+    # Check if the paragraph fits in the available space after adjusting font size
+    
+
+    p = Paragraph(text, style)
+    width_needed, height_needed = p.wrap(available_width, available_height)
+
+    # Draw the frame with a visible boundary for debugging
+    frame = Frame(x, y, available_width, available_height, showBoundary=1, leftPadding=0, rightPadding=0, topPadding=0, bottomPadding=0)
+    frame.addFromList([p], pdf_canvas)
 
 def create_kanban_card_front(pdf_canvas, card_title, image, item_code, orderpage_link, default_supplier, supplier_part_no, x, y):
     card_width, card_height = landscape(A6)
@@ -130,34 +180,38 @@ def create_kanban_card_front(pdf_canvas, card_title, image, item_code, orderpage
     line_x = x + (card_width) / 2  # Center of the card
     pdf_canvas.line(line_x, y, line_x, y + card_height)
 
+    ######################
+    ##### CARD FRONT #####
+    ######################
+
     # Draw card title
-    draw_text_center(pdf_canvas, card_title, 2 * mm, 83 * mm, 70 * mm, 20 * mm)
+    draw_h1_center(pdf_canvas, card_title, 2 * mm, 80 * mm, 70 * mm, 20 * mm)
 
     # Draw item code
     pdf_canvas.setFont("Helvetica", 12)
-    pdf_canvas.drawString(x + 4 * mm, y + card_height - 26 * mm, f"Item Code: {item_code}")
+    item_code_text = "ERP-TeileNr: " + item_code
+    draw_text_center(pdf_canvas,  item_code_text, 2 * mm, 74 * mm, 70 * mm, 5 * mm)
 
     # Draw Image
     image_reader = ImageReader(image)
     pdf_canvas.drawImage(image_reader, 7 * mm, 12 * mm, 60 * mm, 60 * mm)
 
-    # Draw supplier text
-    draw_text_left(pdf_canvas, f"Lieferant: {default_supplier}", 76 * mm, 90 * mm, 70 * mm, 12 * mm)
+    #####################
+    ##### CARD BACK #####
+    #####################
+    pdf_canvas.drawString(76 * mm, 96 * mm, f"Lieferant: {default_supplier}")
 
-    # Draw supplier part number
-    draw_text_left(pdf_canvas, f"L-Teilenummer: {supplier_part_no}", 76 * mm, 82 * mm, 70 * mm, 12 * mm)
+    pdf_canvas.drawString(76 * mm, (96 - 8) * mm, f"L-Teilenummer: {supplier_part_no}")
 
-    # Draw last price label
-    draw_text_left(pdf_canvas, "Bestellmenge:", 76 * mm, 74 * mm, 70 * mm, 12 * mm)
+    pdf_canvas.drawString(76 * mm, (96 - (2*8)) * mm, "Bestellmenge:")
 
-    # Draw date and quantity label
-    draw_text_left(pdf_canvas, "Kanban VE:", 76 * mm, 66 * mm, 70 * mm, 12 * mm)
+    pdf_canvas.drawString(76 * mm, (96 - (3*8)) * mm, "KANBAN VE:")
+    
+    pdf_canvas.drawString(76 * mm, (96 - (4*8)) * mm, "Letzter Preis:")
 
-    # Draw date and quantity label
-    draw_text_left(pdf_canvas, "Letzer Preis:", 76 * mm, 58 * mm, 70 * mm, 12 * mm)
+    pdf_canvas.drawString(76 * mm, (96 - (5*8)) * mm, "Datum        |        Stk.")
 
-    # Draw date and quantity label
-    draw_text_left(pdf_canvas, "Datum   -   Stk.", 76 * mm, 50 * mm, 70 * mm, 12 * mm)
+
 
      # Draw QR Code for order page or orderpage_link text
     if orderpage_link.startswith("http"):
@@ -166,7 +220,7 @@ def create_kanban_card_front(pdf_canvas, card_title, image, item_code, orderpage
         qr_reader = ImageReader(qr_img)
         pdf_canvas.drawImage(qr_reader, (74 + 37 - 15) * mm, y + 6 * mm, 30 * mm, 30 * mm)
     else:
-        draw_text_left(pdf_canvas, orderpage_link, 76 * mm, 25 * mm, 70 * mm, 12 * mm)
+        draw_text_center(pdf_canvas, orderpage_link, 76 * mm, 20 * mm, 70 * mm, 20 * mm)
 
     # Draw QR Code for item_code
     qr_small_size = 150  # Use higher resolution size
@@ -180,8 +234,8 @@ def process_item(item_code):
         title = item_details["item_name"]
         image_url = item_details.get("image", "./default.png")
         orderpage_link = item_details.get("orderpage_link", "")
-        supplier = "Unknown Supplier"
-        supplier_part_no = "Unknown Part No"
+        supplier = "-"
+        supplier_part_no = "-"
         if item_details.get("supplier_items"):
             supplier = item_details["supplier_items"][0]["supplier"]
             supplier_part_no = item_details["supplier_items"][0]["supplier_part_no"]
